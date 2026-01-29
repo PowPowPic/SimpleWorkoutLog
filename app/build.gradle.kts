6f85606d6569f5a7b15a5b5959b67c1d9dfa9e3a
PowPowPic <powder.power.pictures@gmail.com>
2026-01-29 16:34:44 +0900

    SWL31 V8 1.9.1 履歴カード修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 8
-        versionName = "1.9.0"
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
