4b7238ec52b21da49ca956f2f230bc284eacc9cf
PowPowPic <powder.power.pictures@gmail.com>
2026-02-08 15:10:45 +0900

    SWL38 V15 2.0.5 3時ルール文言修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.poweder.simpleworkoutlog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.0.4"
+        versionCode = 15
+        versionName = "2.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
